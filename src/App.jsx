a203b8f0fde8829aa8a2d56633f93cdd866d61b8
leejuyeop <juyeop198@etribe.co.kr>
2025-10-29 16:39:08 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import Layout from './components/Layout/Layout';
-import About from './components/Pages/About/About';
-import Video from './components/Pages/About/Video';
-
-import Contact from './components/Pages/Contact/Contact';
-import Projects from './components/Pages/Projects/Projects';
-import Work from './components/Pages/Work/Work';
+import Layout from './components/layout/Layout';
 
 const App = () => {
-  return (
-    <Layout>
-      <Video />
-      <About />
-      <Work />
-      <Projects />
-      <Contact />
-    </Layout>
-  );
+  return <Layout></Layout>;
 };
 
 export default App;
