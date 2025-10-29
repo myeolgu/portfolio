2c00c52235b4d666304d0d052631181444828fb4
leejuyeop <juyeop198@etribe.co.kr>
2025-10-29 16:47:06 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Main from './pages/Main';
+import Projects from './pages/Projects/Projects';
+import Work from './pages/Work/Work';
 
 const App = () => {
-  return <Layout></Layout>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,21 +8,31 @@ const Main = () => {
   };
 
   return (
-    <section className="about-section">
-      <div className="about-container">
-        <div className="about-text">
-          <p className="txt">{splitText('DREAMERS')}</p>
-          <p className="txt">{splitText('PIONEERS')}</p>
-          <p className="txt">{splitText('INNOVATORS')}</p>
-        </div>
+    <section className="wrap">
+      {/* 인트로 섹션 */}
+      <section className="video-section">
+        <video autoPlay loop muted playsInline className="about-video">
+          <source src={require('../assets/videos/video.mp4')} type="video/mp4" />
+        </video>
+      </section>
 
-        <div className="about-description">
-          Nexon is a leader in global entertainment with a portfolio of successful game franchises enjoyed by hundreds of millions of people around the world. Nexon has more than 40 live games in
-          operation in more than 190 countries, available on PC, console and mobile. Nexon has more than ten intellectual properties including MapleStory, Mabinogi, and Dungeon&amp;Fighter supported
-          by best-in-class Live Operations which keeps the games balanced, and refreshed with new content and experiences. Spanning multiple generations, Nexon games are enjoyed for decades by players
-          all over the world.
+      {/* 소개 섹션 */}
+      <section className="about-section">
+        <div className="about-container">
+          <div className="about-text">
+            <p className="txt">{splitText('DREAMERS')}</p>
+            <p className="txt">{splitText('PIONEERS')}</p>
+            <p className="txt">{splitText('INNOVATORS')}</p>
+          </div>
+
+          <div className="about-description">
+            Nexon is a leader in global entertainment with a portfolio of successful game franchises enjoyed by hundreds of millions of people around the world. Nexon has more than 40 live games in
+            operation in more than 190 countries, available on PC, console and mobile. Nexon has more than ten intellectual properties including MapleStory, Mabinogi, and Dungeon&amp;Fighter supported
+            by best-in-class Live Operations which keeps the games balanced, and refreshed with new content and experiences. Spanning multiple generations, Nexon games are enjoyed for decades by
+            players all over the world.
+          </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
