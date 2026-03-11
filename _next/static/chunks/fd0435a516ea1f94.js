b45fb1755681772251d723d30a07df78591f7521
myeolgu <juyeop198@naver.com>
2026-03-11 17:24:30 +0900

    Updates

---
+++ _next/static/chunks/fd0435a516ea1f94.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/9d14e85d2c2a4fde.js",
+  "static/chunks/0f06c37a4792e4cf.js",
+  "static/chunks/483fd633092d5318.js",
+  "static/chunks/456d374e97311baf.js",
+  "static/chunks/ae4d4e67228fe32d.css",
+  "static/chunks/turbopack-a005fc6b5f15e024.js"
+])
---
+++ _next/static/jIaUsU79mjUYr4Cu8ve8Z/_buildManifest.js
@@ -0,0 +1,23 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/80a2d166ce4d5476.js"
+  ],
+  "/_error": [
+    "static/chunks/c3d6e58af8eb883b.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio//_next/:path+",
+        "destination": "/portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
