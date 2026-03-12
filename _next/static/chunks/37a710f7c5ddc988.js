99b652255611f97782944b706b52ba5de589d534
myeolgu <juyeop198@naver.com>
2026-03-12 15:29:53 +0900

    Updates

--- _next/static/chunks/37a710f7c5ddc988.js
+++ _next/static/chunks/37a710f7c5ddc988.js
@@ -1,8 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/3eacf086e34e68f3.js",
+  "static/chunks/3a38af6dc1935f40.js",
   "static/chunks/0f06c37a4792e4cf.js",
   "static/chunks/483fd633092d5318.js",
   "static/chunks/918e744fae8dfa0f.js",
   "static/chunks/80bfa0465b31bfc9.css",
-  "static/chunks/turbopack-5386da19c62c6304.js"
+  "static/chunks/turbopack-9c3e249efea4e096.js"
 ])
